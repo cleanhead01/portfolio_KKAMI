cf76b3932fb74a2952ea2463e1eaadc8a9f21819
cleanhead01 <seokjh93@naver.com>
2020-09-11 15:38:24 +0900

    제발

--- js/common.js
+++ js/common.js
@@ -21,33 +21,12 @@
     })
 
     // 각각 서브페이지 안에서 페이지 이동 연결
-    $('#containerBox .center > .button > a').on('click', function(e){
+    $('#containerBox .button > a').on('click', function(e){
         e.preventDefault()
         var url = $(this).attr('href')
         $('#container').remove()
         $('#containerBox').load(url)
     })
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
