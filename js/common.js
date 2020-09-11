7adbd7c5f9b72d8c3403c12543a471aac4298c88
cleanhead01 <seokjh93@naver.com>
2020-09-11 18:20:59 +0900

    슬라이드 준비

--- js/common.js
+++ js/common.js
@@ -70,26 +70,6 @@
 
 
 
-    // $('.slide_inner').slick({
-    //     autoplay: true,
-    //     dots: true,
-    //     autoplaySpeed: 4500,
-    //     speed: 600,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     pauseOnHover: true,
-    //     pauseOnDotsHover: false,
-    //     pauseOnFocus: false,
-    //     cssEase: 'ease',
-    //     draggable: true,
-    //     fade: false,
-    //     arrows: true,
-    //     // prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
-    //     // nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
-        
-
-    // })
-    
 
 
 
