037624d10f47d3b2c0bb5850f6b7a5c2c553dc72
cleanhead01 <seokjh93@naver.com>
2020-09-11 15:08:00 +0900

    테스트

--- js/common.js
+++ js/common.js
@@ -11,6 +11,44 @@
         $('#containerBox').load(url)
     })
 
+
+    $('.nav_box .nav a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#container').remove()
+        $('#containerBox').load(url)
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // depth1에 호버하면 depth2 메뉴
     $('.depth1 li').hover(
         function(){
@@ -29,25 +67,46 @@
 
 
 
-    $('.slide_inner').slick({
-        autoplay: true,
-        dots: true,
-        autoplaySpeed: 4500,
-        speed: 600,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: true,
-        pauseOnDotsHover: false,
-        pauseOnFocus: false,
-        cssEase: 'ease',
-        draggable: true,
-        fade: false,
-        arrows: true,
-        // prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
-        // nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $('.slide_inner').slick({
+    //     autoplay: true,
+    //     dots: true,
+    //     autoplaySpeed: 4500,
+    //     speed: 600,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     pauseOnHover: true,
+    //     pauseOnDotsHover: false,
+    //     pauseOnFocus: false,
+    //     cssEase: 'ease',
+    //     draggable: true,
+    //     fade: false,
+    //     arrows: true,
+    //     // prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
+    //     // nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
         
 
-    })
+    // })
     
 
 
