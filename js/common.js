1326e16ca4cd436ac815571b7fe96d05acd6d0ef
cleanhead01 <seokjh93@naver.com>
2020-09-11 15:27:23 +0900

    다시 테스트

--- js/common.js
+++ js/common.js
@@ -21,7 +21,7 @@
     })
 
     // 각각 서브페이지 안에서 페이지 이동 연결
-    $('#container .center > .button > a').on('click', function(e){
+    $('#containerBox .center > .button > a').on('click', function(e){
         e.preventDefault()
         var url = $(this).attr('href')
         $('#container').remove()
