ce65fcbcb150ab12a96b50eac4f4782eb8c257fd
cleanhead01 <seokjh93@naver.com>
2020-09-16 18:12:29 +0900

    09-16 - 06:12 진행중

--- js/sub-page.js
+++ js/sub-page.js
@@ -1,11 +1,21 @@
 (function($){
 
 // 각각 서브페이지 안에서 페이지 이동 연결
-$('#container .center .button > a').on('click', function(e){
+// $('#container .center .button > a').on('click', function(e){
+//     e.preventDefault()
+//     var url = $(this).attr('href')
+//     $('#container').remove()
+//     $('#containerBox').load(url)
+// })
+
+
+$('#notice_back .center .button a').on('click', function(e){
     e.preventDefault()
-    var url = $(this).attr('href')
-    $('#container').remove()
-    $('#containerBox').load(url)
+    var index = $(this).parent().index()
+    $(this).addClass('on')
+    .siblings().removeClass('on')
+    $('#no_qa .div_qa, #no_qa .div_no').eq(index).fadeIn(100)
+    .siblings().fadeOut(100)
 })
 
 
